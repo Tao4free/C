5ffc1079b683e656818565ddd4a0504ee8f4ff9f
tao4free <takizawa_tao@live.com>
2018-06-27 16:50:05 +0900

    20180627_1649_cygwin

---
+++ The_C_Programming_Language_Second_Edition_Exercise/Chapter01/Exercise_1-1/hello.c
@@ -0,0 +1,13 @@
+/*
+ * Author:Tao Lu <hakufu.asano@gmail.com>
+ *
+ * Solution for Exercise_1-01, Chapter01 
+ */
+
+#include <stdio.h>
+
+int main(void){
+	printf("hello, world\n");
+
+	return 0; /* or exit(0) */
+}
