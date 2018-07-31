d068f3f13fb3f8edb179c3ee50cea8cea8e9e0f1
tao4free <takizawa_tao@live.com>
2018-07-31 15:05:26 +0900

    test

--- The_C_Programming_Language_Second_Edition_Exercise/Chapter01/Exercise_1-13/count_digits.c
+++ The_C_Programming_Language_Second_Edition_Exercise/Chapter01/Exercise_1-13/count_digits.c
@@ -6,44 +6,33 @@
 
 #include <stdio.h>
 
-#define IN 1	/* inside a word */
-#define OUT 0	/* outside a word */
-
-/* bare-bones version of the UNIX program wc 
- * count lines, words, and chracters in input
- * write one word one line
+/* count digits, white space, others 
  */
 
 int
 main(void)
 {
-	int c, nl, nw, nc, state, nout;
+	int c, i, nwhite, nother;
+	int ndigit[10];
 
-	state = OUT;
-	nl = nw = nc = nout = 0;
+	nwhite = nother = 0;
+	for (i = 0; i < 10; ++i)
+		ndigit[i] = 0;
+
 	while ( (c = getchar()) != EOF) {
-		++nc;
-		if (c == '\n')
-	      		++nl;
-		if (c == ' ' || c == '\n' || c == '\t') {
-	      		state = OUT;
-			++nout;
-		}
-		else if (state == OUT) {
-	      		state = IN;
-	      		++nw;
-			nout = 0;
-		}
-		if (state == IN)
-	      		putchar(c);
-		if (state == OUT && nout == 1 )
-	      		putchar('\n');
+		if (c >= '0' && c <= '9')
+			++ndigit[c-'0'];
+		else if (c == ' ' || c == '\n' || c == '\t')
+			++nwhite;
+		else
+			++nother;
+
+	printf("digits =");
+	for (i = 0; i < 10; ++i)
+		printf(" %d", ndigit[i]);
+	printf(", white space = %d, other = %d\n",
+			nwhite, nother);
 	}
 
-	putchar('\n');
-	printf("number of newline: %d\n", nl);
-	printf("number of words: %d\n", nw);
-	printf("number of characters: %d\n", nc);
-	
 	return 0; 
 }
